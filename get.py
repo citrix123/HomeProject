2c298cf6debb5d191f44c63daa6f888be24121b7
Rahul Jain <talentediq@gmail.com>
2017-08-19 18:31:05 +0530

    Added Face Detection using Harcascade TODO: 1. Training data 2. Testing data

--- get.py
+++ get.py
@@ -1,52 +1,54 @@
 import numpy as np
 import cv2
 import time
-cap = cv2.VideoCapture(1)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+path = '/home/citrix/Documents/Projects/opencv/data/haarcascades/'
+face_cascade = cv2.CascadeClassifier(path + 'haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.CascadeClassifier(path + 'haarcascade_eye.xml')
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def initVideoDevice(capDevice):
+    return cv2.VideoCapture(1)
 
-    # gray = cv2.bilateralFilter(gray, 11, 17, 17)
-    edged = cv2.Canny(gray, 30, 200)
 
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    # ret,thresh = cv2.threshold(blurred,127,255,cv2.THRESH_TOZERO_INV)
+def renderCamera(cap):
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,11,2)
-    # ret,thresh = cv2.threshold(thresh, 60, 255, cv2.THRESH_BINARY)
-    # Display the resulting frame
-    cv2.imshow('frame',th2)
-    # cv2.imshow('blurred',blurred)
-    # laplacian = cv2.Laplacian(thresh,cv2.CV_64F)
-    # cv2.imshow('frame2',blurred)
-    # (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-    # screenCnt = None
-    # for c in cnts:
-    #     peri = cv2.arcLength(c , True)
-    #     approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    # 	# if our approximated contour has four points, then
-    # 	# we can assume that we have found our screen
-    # 	if len(approx) > 11:
-    # 		screenCnt = approx
-    # 		break
-    #
-    # # mask = np.zeros_like(frame)
-    # cv2.drawContours(frame, [screenCnt], -1, (0, 255, 0), 3)
-    # print screenCnt
-    # out = np.zeros_like(frame)
-    # out[mask == 255] = frame[mask == 255]
-    # cv2.imshow("Game Boy Screen", frame)
-    # cv2.imshow("output", out)
-    # im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        edged = cv2.Canny(gray, 30, 200)
+
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+                cv2.THRESH_BINARY,11,2)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
+	for (x,y,w,h) in faces:
+	    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+	    roi_gray = gray[y:y+h, x:x+w]
+	    roi_color = frame[y:y+h, x:x+w]
+	    showFrame('crop', roi_gray)
+        
+	showFrame('img',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+def showFrame(name , frameObj):
+    cv2.imshow(name, frameObj)
+
+
+def destroyDevice(arg):
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def main(device):
+    renderCamera(initVideoDevice(0))
+
+if __name__ == '__main__':
+    main(0)
